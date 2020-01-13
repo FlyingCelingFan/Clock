195b4c83930929f72c0aa5badb02de9835801a50
FlyingCelingFan <57369474+FlyingCelingFan@users.noreply.github.com>
2020-01-12 22:48:13 -1000

    added javascript time

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-/*
+  /*
 notes:
 make a clock face
 button dies
+
+make coundown clock that sets time for the amount it is pressed down
 
 */ 
 
 var currentTime = new Date();
 
-var currentHours
-var currentMinute
-var currentSecond
-var currentMiliSecond
+var currentHour currentTime.getHours ();
+var currentMinute currentTime.getMinutes ();
+var currentSecond currentTime.getSeconds ();
+var currentMilisecond currentTime.getMiliseconds ();
+
+currentMinute = (currentMinute < 10 ? "0" : "" ) + currentMinute;
+currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond;
+currentMilisecond = (currentMilisecond < 10 ? "0" : "") + currentMilisecond;
+
+var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSecond + " " + timeOfDay;
+
