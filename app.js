6a9f59a1c8aae18e6db3fe382df15dd2fbe61081
FlyingCelingFan <NarcisoAndrewP@gmail.com>
2020-01-17 10:08:08 -1000

    Finished

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function showTime(){
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
+  var time = "AM";
 
   if(h == 0){
       h = 12;
@@ -23,14 +23,16 @@ function showTime(){
 
   if(h > 12){
       h = h - 12;
-      session = "PM";
+      time = "PM";
   }
+
+  (h)
 
   h = (h < 10) ? "0" + h : h;
   m = (m < 10) ? "0" + m : m;
   s = (s < 10) ? "0" + s : s;
 
-  var time = h + ":" + m + ":" + s + " " + session;
+  var time = h + ":" + m + ":" + s + " " + time;
   document.getElementById("javascropt").innerText = time;
   document.getElementById("javascropt").textContent = time;
 
