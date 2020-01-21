cfe0c4c7213b651870eb598870188a0a152285e6
FlyingCelingFan <57369474+FlyingCelingFan@users.noreply.github.com>
2020-01-20 15:40:17 -1000

    Finished.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ make coundown clock that sets time for the amount it is pressed down
 function showTime(){
   var date = new Date();
   var h = date.getHours();
-  var m = date.getMinutes();
+  var m = date.getMinutes(); 
   var s = date.getSeconds();
   var time = "AM";
 
@@ -21,7 +21,11 @@ function showTime(){
       h = 12;
   }
 
-  h = (h > 12) ? time = "PM" : time ="AM";
+  if(h > 12){
+      h = h - 12;
+      time = "PM"; 
+  }
+
 
   h = (h < 10) ? "0" + h : h;
   m = (m < 10) ? "0" + m : m;
@@ -40,13 +44,13 @@ showTime();
 
 function button() {
 
-  alert("alert");
+  alert("this is the end of the presentation.");
 
 }
 
 
-
 /*
+MAIN CODE
 
 function updateClock() {
 
@@ -57,11 +61,11 @@ var currentTime = new Date();
 var currentHour = currentTime.getHours ();
 var currentMinute = currentTime.getMinutes ();
 var currentSecond = currentTime.getSeconds ();
-var currentMilisecond = currentTime.getMiliseconds ();
+//var currentMilisecond = currentTime.getMiliseconds ();
 
-currentMinute = (currentMinute < 10 ? "0" : "" ) + currentMinute;
+currentMinute = (currentMinute < 10 ? "0" : "") + currentMinute;
 currentSecond = (currentSecond < 10 ? "0" : "") + currentSecond;
-currentMilisecond = (currentMilisecond < 10 ? "0" : "") + currentMilisecond;
+//currentMilisecond = (currentMilisecond < 10 ? "0" : "") + currentMilisecond;
 
 
 //shorthand if statement.
@@ -73,10 +77,12 @@ currentHour	= ( currentHour	== 0 ) ? 12 : currentHour;
 
 //This is what the user sees
 //ex: Hour:Minute:Second:Milisecond AM/PM
-var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSecond + ":" + currentMilisecond + " " + timeOfDay;
+var currentTimeString = currentHour + ":" + currentMinute + ":" + currentSecond + /*":" + currentMilisecond +*/// " " + timeOfDay;
 
 //shows the clock in index.html
-document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+//document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+/*document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+
 
 }
 
@@ -99,19 +105,19 @@ function updateClock ( )
   currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
   // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+  var timeOfDay = ( currentHours < 12 ) ? "Agit commit  : "PM";
 
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+  // Convert the hours component to 12-hour git commit rmat if needed
+  currentHours = ( currentHours > 12 ) ? curgit commit ntHours - 12 : currentHours;
 
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+  // Convert an hours component of "0" to "1git commit 
+  currentHours = ( currentHours == 0 ) ? 12 git commit currentHours;
 
   // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+  var currentTimeString = currentHours + ":"git commit  currentMinutes + ":" + currentSeconds + " " + timeOfDay;
 
   // Update the time display
-  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+  document.getElementById("clock").firstChilgit commit nodeValue = currentTimeString;
 }
 
 
